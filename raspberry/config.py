8da1d427ac025cbcd3c31b2f7a54a0a182253a0b
bruno <brunosilvamoraes2006@gmail.com>
2025-10-05 14:40:05 -0300

    ssh credentials config

---
+++ raspberry/config.py
@@ -0,0 +1,6 @@
+# For general configuration settings
+
+msg_error = "[ \033[91mError\033[0m ]"
+msg_ok = "[ \033[92mOk\033[0m ]"
+msg_warning = "[ \033[93mWarning\033[0m ]"
+msg_info = "[ \033[94minfo\033[0m ]"
--- raspberry/ssh_tunnel.py
+++ raspberry/ssh_tunnel.py
@@ -1,8 +1,7 @@
 from sshtunnel import SSHTunnelForwarder
 from dotenv import load_dotenv
+from config import *
 import time, sys, os
-
-load_dotenv()  
 
 # Autenticação por senha. Trocar para chave ssh posteriomente
 class SSH_Connection:
@@ -15,7 +14,7 @@ class SSH_Connection:
         self.tunnel = None
         self.default_port = 22
     
-    def start_tunnel(self) -> bool:
+    def start_tunnel(self):
         try:
             self.tunnel = SSHTunnelForwarder(
                 (self.server_ip, self.default_port),
@@ -25,32 +24,46 @@ class SSH_Connection:
                 local_bind_address=('127.0.0.1', self.local_port),
             )
             self.tunnel.start()
-            print(f"SSH tunnel established: localhost:{self.local_port} → {self.server_ip}:{self.remote_port}")
+            print(f"{msg_ok} SSH tunnel established: localhost:{self.local_port} → {self.server_ip}:{self.remote_port}")
 
         except Exception as e:
-            print(f"Failed to start SSH tunnel or run command: {e}")
+            print(f"{msg_error} Failed to start SSH tunnel or run command: {e}")
             sys.exit(1)
+
+    def stop_tunnel(self):
+        if self.tunnel and self.tunnel.is_active:
+            self.tunnel.stop()
+            print(f"{msg_warning} ssh tunnel closed")
+        else:
+            print(f"{msg_warning} no active ssh tunnel to close")
 
     def main(self): # edição futura
         try:
             self.start_tunnel()
-            print(" Connection established and ready.")
-            while True:
-                time.sleep(1)
+            print(f"{msg_ok} connection established and ready")
+
         except KeyboardInterrupt:
-            print("\nInterrupted by user.")
+            print("\noperation interrupted by user")
+
         except Exception as e:
-            print(f"Error during SSH operation: {e}")
+            print(f"{msg_error} fatal error during ssh operation: {e}")
         finally:
-            self.tunnel.stop()
+            self.stop_tunnel()
 
 if __name__ == "__main__":
-    SERVER_IP = os.environ.get("SSH_SERVER_IP")
-    USER = os.environ.get("SSH_USER")
-    PASSWORD = os.environ.get("SSH_PASSWORD")
-    REMOTE_PORT = int(os.environ.get("REMOTE_MQTT_PORT", 1883))
-    LOCAL_PORT = int(os.environ.get("LOCAL_MQTT_PORT", 1883))
+    load_dotenv(dotenv_path="venv/credentials.env")
+    SERVER_IP = os.getenv("SSH_SERVER_IP")
+    USER = os.getenv("SSH_USER")
+    PASSWORD = os.getenv("SSH_PASSWORD")
+    REMOTE_PORT = int(os.getenv("REMOTE_PORT", 1883))
+    LOCAL_PORT = int(os.getenv("LOCAL_PORT", 1883))
 
+    print(f"{msg_info} server IP:", SERVER_IP)
+    print(f"{msg_info} user:", USER)
+    print(f"{msg_info} password:", PASSWORD)
+    print(f"{msg_info} remote port:", REMOTE_PORT)
+    print(f"{msg_info} local port:", LOCAL_PORT)
+    
     connection = SSH_Connection(
         server_ip=SERVER_IP,
         ssh_user=USER,
