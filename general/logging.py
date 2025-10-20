dc021771543aed27ca98b1386c4df0bdd00ec159
bruno <brunosilvamoraes2006@gmail.com>
2025-10-20 11:31:16 -0300

    ajuste no logging.py para criar um diretorio de logs

--- general/logging.py
+++ general/logging.py
@@ -1,13 +1,14 @@
-import sys
-import logging
+import sys, os, logging
+
+os.makedirs("general/logs", exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,  
     format="%(asctime)s [%(levelname)s] %(message)s",  
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
-        logging.FileHandler("logs/ssh_connection.log"),  
-        logging.StreamHandler(sys.stdout)       
-    ]
-)
-logger = logging.getLogger(__name__)
+        logging.FileHandler("general/logs/mqtt.log", encoding="utf-8"), 
+        logging.StreamHandler(sys.stdout)
+        ]
+    )
+logger = logging.getLogger("MQTT")
